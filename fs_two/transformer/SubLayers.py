73d68c0f9e7064072c36488e81293b88879f7595
Michael Pavlukhin <alivemihail@gmail.com>
2021-03-23 19:05:45 +0300

    Update SubLayers.py

--- fs_two/transformer/SubLayers.py
+++ fs_two/transformer/SubLayers.py
@@ -137,7 +137,7 @@ class PositionwiseFeedForward(nn.Module):
     def forward(self, x):
         residual = x
         output = x.transpose(1, 2)
-        output = self.w_2(swiglu_fn(self.w_1(output)))
+        output = self.w_2(F.relu(self.w_1(output)))
         output = output.transpose(1, 2)
         output = self.dropout(output)
         output = self.layer_norm(output + residual)
