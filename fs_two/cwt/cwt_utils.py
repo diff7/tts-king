c6fbb5b6d554c0f23ab5fb81b1f09402c947a2db
Egorsv <geksut@gmail.com>
2021-07-24 19:37:37 -0300

    batch inverse

--- fs_two/cwt/cwt_utils.py
+++ fs_two/cwt/cwt_utils.py
@@ -1,10 +1,11 @@
-from sklearn import preprocessing
-import pycwt as wavelet
+import torch
 import numpy as np
+import pycwt as wavelet
+from sklearn import preprocessing
 
 
 def mse(a, b):
-    return ((a-b)**2).mean()
+    return ((a - b) ** 2).mean()
 
 
 # PREPROCESSING
@@ -14,17 +15,44 @@ def transform_cwt(lf0, J=10):
     mother = wavelet.MexicanHat()
     dt = 0.005
     dj = 1
-    s0 = dt*2
+    s0 = dt * 2
     Wavelet_lf0, scales, freqs, coi, fft, fftfreqs = wavelet.cwt(
-        np.squeeze(lf0), dt, dj, s0, J, mother)
+        np.squeeze(lf0), dt, dj, s0, J, mother
+    )
     Wavelet_lf0 = np.real(Wavelet_lf0).T
     return Wavelet_lf0
 
 
-def inverse_cwt(Wavelet_lf0, scales=10):
-    lf0_rec = np.zeros([Wavelet_lf0.shape[0], len(scales)])
+def inverse_cwt(wavelet_coefs, scales=[]):
+    lf0_rec = np.zeros([wavelet_coefs.shape[0], len(scales)])
     for i in range(0, len(scales)):
-        lf0_rec[:, i] = Wavelet_lf0[:, i]*((i+1+2.5)**(-2.5))
+        lf0_rec[:, i] = wavelet_coefs[:, i] * ((i + 1 + 2.5) ** (-2.5))
     lf0_rec_sum = np.sum(lf0_rec, axis=1)
     lf0_rec_sum = preprocessing.scale(lf0_rec_sum)
+    return lf0_rec_sum
+
+
+# TO REVERSE ADD
+# reverse  = inverse_batch_cwt(wavelet_coefs, scales=10)*std + mean
+
+
+def scaler(tensor, axis=-1):
+    mean = tensor.mean(axis)
+    std = tensor.std(axis)
+    print(mean)
+    res = (tensor - mean.reshape(-1, 1)) / std.reshape(-1, 1)
+    return res
+
+
+def inverse_batch_cwt(wavelet_coefs, scales=10):
+    batch_size = wavelet_coefs.shape[0]
+    length = wavelet_coefs.shape[1]
+    lf0_rec = torch.zeros(
+        [batch_size, length, len(scales)], dtype=torch.float32
+    )
+    for i in range(0, len(scales)):
+        lf0_rec[:, :, i] = wavelet_coefs[:, :, i] * ((i + 1 + 2.5) ** (-2.5))
+    lf0_rec_sum = torch.sum(lf0_rec, axis=-1)
+    lf0_rec_sum = scaler(lf0_rec_sum)
+    print(lf0_rec_sum[0][:10])
     return lf0_rec_sum
