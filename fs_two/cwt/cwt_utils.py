f23cab8485cb0c7dc40660c747a116b7f9a4de95
Michael Pavlukhin <alivemihail@gmail.com>
2021-08-01 14:26:04 +0300

    Update cwt_utils.py

--- fs_two/cwt/cwt_utils.py
+++ fs_two/cwt/cwt_utils.py
@@ -54,7 +54,7 @@ scaler_tc = TorchStandardScaler()
 def inverse_batch_cwt(wavelet_coefs, num_scales=10):
     batch_size = wavelet_coefs.shape[0]
     length = wavelet_coefs.shape[1]
-    lf0_rec = torch.zeros([batch_size, length, num_scales], dtype=torch.float32)
+    lf0_rec = torch.zeros([batch_size, length, num_scales], dtype=torch.float32).to(wavelet_coefs.device)
     for i in range(0, num_scales):
         lf0_rec[:, :, i] = wavelet_coefs[:, :, i] * ((i + 1 + 2.5) ** (-2.5))
     lf0_rec_sum = torch.sum(lf0_rec, axis=-1)
